e18a180579eef5d35a57c2e671ff2c25f5a7de55
Facundo Maero <facundojmaero@gmail.com>
2016-10-11 23:06:14 -0300

    No usamos mas execvp

--- shell/work/code/src/launch.c
+++ shell/work/code/src/launch.c
@@ -2,6 +2,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+const char *paths[] = {
+  "/bin/",
+  "/usr/bin/",
+  "/usr/games/",
+  "/usr/sbin/"
+};
+
+int isRelative(char** path){
+
+	if(strstr(*path,"/")==NULL){
+		return 1;
+	}
+	return 0;
+}
+
+int num_paths() {
+  return sizeof(paths) / sizeof(char *);
+}
+
+int runCommand(char **args, char *path){
+  	if (execv(path, args) == -1) 
+    	return -1;
+    return 0;
+}
 
 int launch(char **args)
 {
@@ -12,14 +38,33 @@ int launch(char **args)
   if (pid == 0) {
     // Child process
 
-    if (execvp(args[0], args) == -1) {
-      perror("lsh");
-    }
-    exit(EXIT_FAILURE);
+    // if (execvp(args[0], args) == -1) {
+    //   perror("Baash error");
+    // }
+    // exit(EXIT_FAILURE);
+
+  	if (isRelative(args)){
+  		char new_str[50] = "";
+  		for(int i=0; i<num_paths();i++){
+  			strcat(new_str,paths[i]);
+  			strcat(new_str,args[0]);
+  			runCommand(args,new_str);
+
+  			//limpio el string para probar de nuevo
+  			new_str[0] = 0;
+  		}
+  		perror("Baash error");
+  		exit(EXIT_FAILURE);
+  	}
+
+  	else if(runCommand(args,args[0]) == -1){
+		perror("Baash error");
+	    exit(EXIT_FAILURE);
+  	}
   } 
   else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("Baash error forking");
   } 
 
   else {
@@ -31,4 +76,3 @@ int launch(char **args)
 
   return 1;
 }
-
