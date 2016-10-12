534f3a9676c33fd8189123759b7c7771540dbbe7
Facundo Maero <facundojmaero@gmail.com>
2016-10-12 11:34:14 -0300

    Ahora hace cosas en paralelo con & y encuentra | < >

--- shell/work/code/src/launch.c
+++ shell/work/code/src/launch.c
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define AMP 0
+#define PIPE 1
+#define INPT 2
+#define OUTP 3
+
+int flags [4] = {0,0,0,0};
+
 const char *paths[] = {
   "/bin/",
   "/usr/bin/",
@@ -29,19 +36,45 @@ int runCommand(char **args, char *path){
     return 0;
 }
 
+
+int checkFlag(char ch, char* word, int flagIndex){
+
+	if(strcmp(&ch,word)==0){
+  		flags[flagIndex]++;
+  		return 0;
+  	}
+  	return -1;
+}
+
+void resetFlags(){
+	for (int i = 0; i < 4; ++i)
+		flags[i]=0;
+}
+
 int launch(char **args)
 {
   pid_t pid, wpid;
   int status;
+  int i=0;
+
+  resetFlags();
+
+  char caracter[5]= "&|<>";
+
+  while(args[i]!=NULL){
+
+  	for(int j=0; j<4; j++){
+	  	if(checkFlag(caracter[j],args[i],j)==0){
+	  		args[i]=NULL;
+	  		break;
+	  	}
+	}
+  	i++;
+  }
 
   pid = fork();
   if (pid == 0) {
     // Child process
-
-    // if (execvp(args[0], args) == -1) {
-    //   perror("Baash error");
-    // }
-    // exit(EXIT_FAILURE);
 
   	if (isRelative(args)){
   		char new_str[50] = "";
@@ -69,9 +102,12 @@ int launch(char **args)
 
   else {
     // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+  	if(flags[AMP] == 0){
+	    do {
+	      wpid = waitpid(pid, &status, WUNTRACED);
+	    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
   }
 
   return 1;
